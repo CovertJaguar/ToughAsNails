7fc5816c29f27361864ceac70f351d28ade1682f
Vexatos <Stuarzt@gmx.de>
2017-05-03 20:52:00 +0200

    Fixed isRaining and season time.

--- src/main/java/toughasnails/util/TimeUtil.java
+++ src/main/java/toughasnails/util/TimeUtil.java
@@ -7,8 +7,8 @@ public class TimeUtil {
 
 	public static final class F {
 
-		public static final float TICKS_IN_DAY = 36000.0F;
-		public static final float TICKS_IN_NIGHT = 24000.0F;
+		public static final float TICKS_IN_DAY = 12000.0F;
+		public static final float TICKS_IN_NIGHT = 12000.0F;
 		public static final float TICKS_TOTAL = TICKS_IN_DAY + TICKS_IN_NIGHT;
 		public static final float TICKS_TO_NOON = TICKS_IN_DAY / 2.0F;
 		public static final float TICKS_TO_MIDNIGHT = TICKS_IN_NIGHT / 2.0F;
@@ -16,8 +16,8 @@ public class TimeUtil {
 
 	public static final class I {
 
-		public static final int TICKS_IN_DAY = 36000;
-		public static final int TICKS_IN_NIGHT = 24000;
+		public static final int TICKS_IN_DAY = 12000;
+		public static final int TICKS_IN_NIGHT = 12000;
 		public static final int TICKS_TOTAL = TICKS_IN_DAY + TICKS_IN_NIGHT;
 		public static final int TICKS_TO_NOON = TICKS_IN_DAY / 2;
 		public static final int TICKS_TO_MIDNIGHT = TICKS_IN_NIGHT / 2;
